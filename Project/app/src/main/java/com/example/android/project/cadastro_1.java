f20fe1bbfab1c5ffff368ff792b18c8b43546d48
allanericson <allanericson@gmail.com>
2017-12-06 01:20:38 -0300

    update

---
+++ Project/app/src/main/java/com/example/android/project/cadastro_1.java
@@ -0,0 +1,13 @@
+package com.example.android.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cadastro_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_1);
+    }
+}
