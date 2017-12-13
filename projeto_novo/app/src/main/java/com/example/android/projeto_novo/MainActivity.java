48cc0f081ed3d6f9ea5f2154b4e2b74dc1c25adb
allanericson <allanericson@gmail.com>
2017-12-13 20:30:22 -0300

    ATT

---
+++ projeto_novo/app/src/main/java/com/example/android/projeto_novo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.projeto_novo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+compile
+
+import java.io.IOException;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
